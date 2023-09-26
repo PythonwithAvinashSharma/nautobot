PythonwithAvinashSharma/nautobot

commit 8507212c7da9cb18ba4824fd20dcb8f0349748c9

Fix #4476 - add /api/extras/jobs/NAME/ REST API endpoints (#4517)

nautobot/extras/api/views.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter
 from graphene_django.views import GraphQLView
 from graphql import GraphQLError
 from rest_framework import status
@@ -24,6 +24,8 @@ from nautobot.core.api.views import (
     BulkDestroyModelMixin,
     BulkUpdateModelMixin,
     ModelViewSet,
+    ModelViewSetMixin,
+    NautobotAPIVersionMixin,
     ReadOnlyModelViewSet,
 )
 from nautobot.core.exceptions import CeleryWorkerNotRunningException
@@ -85,11 +87,11 @@ class NotesViewSetMixin:
         responses={201: serializers.NoteSerializer(many=False)},
     )
     @action(detail=True, url_path="notes", methods=["get", "post"])
-    def notes(self, request, pk=None):
+    def notes(self, request, *args, **kwargs):
         """
         API methods for returning or creating notes on an object.
         """
-        obj = get_object_or_404(self.queryset, pk=pk)
+        obj = get_object_or_404(self.queryset, **{self.lookup_field: self.kwargs[self.lookup_url_kwarg]})
         if request.method == "POST":
             content_type = ContentType.objects.get_for_model(obj)
             data = request.data
@@ -428,16 +430,14 @@ def _create_schedule(serializer, data, job_model, user, approval_required, task_
     return scheduled_job
 
 
-class JobViewSet(
-    # DRF mixins:
+class JobViewSetBase(
+    NautobotAPIVersionMixin,
     # note no CreateModelMixin
+    mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
-    # Nautobot mixins:
-    BulkUpdateModelMixin,
-    BulkDestroyModelMixin,
-    # Base class
-    ReadOnlyModelViewSet,
+    ModelViewSetMixin,
+    viewsets.GenericViewSet,
     NotesViewSetMixin,
 ):
     queryset = Job.objects.all()
@@ -446,7 +446,7 @@ class JobViewSet(
 
     @extend_schema(responses={"200": serializers.JobVariableSerializer(many=True)})
     @action(detail=True, filterset_class=None)
-    def variables(self, request, pk):
+    def variables(self, request, *args, **kwargs):
         """Get details of the input variables that may/must be specified to run a particular Job."""
         job_model = self.get_object()
         job_class = job_model.job_class
@@ -506,7 +506,7 @@ class JobViewSet(
         permission_classes=[JobRunTokenPermissions],
         parser_classes=[JSONParser, MultiPartParser],
     )
-    def run(self, request, *args, pk, **kwargs):
+    def run(self, request, *args, **kwargs):
         """Run the specified Job."""
         job_model = self.get_object()
         if not request.user.has_perm("extras.run_job"):
@@ -653,6 +653,43 @@ class JobViewSet(
         if job_result:
             data["job_result"] = serializers.JobResultSerializer(job_result, context={"request": request}).data
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class JobViewSet(
+    JobViewSetBase,
+    mixins.ListModelMixin,
+    BulkUpdateModelMixin,
+    BulkDestroyModelMixin,
+):
+    lookup_value_regex = r"[-0-9a-fA-F]+"
+
+
+@extend_schema_view(
+    destroy=extend_schema(operation_id="extras_jobs_destroy_by_name"),
+    partial_update=extend_schema(operation_id="extras_jobs_partial_update_by_name"),
+    notes=extend_schema(methods=["get"], operation_id="extras_jobs_notes_list_by_name"),
+    retrieve=extend_schema(operation_id="extras_jobs_retrieve_by_name"),
+    run=extend_schema(
+        methods=["post"],
+        operation_id="extras_jobs_run_create_by_name",
+        request={
+            "application/json": serializers.JobInputSerializer,
+            "multipart/form-data": serializers.JobMultiPartInputSerializer,
+        },
+        responses={"201": serializers.JobRunResponseSerializer},
+    ),
+    update=extend_schema(operation_id="extras_jobs_update_by_name"),
+    variables=extend_schema(operation_id="extras_jobs_variables_list_by_name"),
+)
+@extend_schema_view(
+    notes=extend_schema(methods=["post"], operation_id="extras_jobs_notes_create_by_name"),
+)
+class JobByNameViewSet(
+    JobViewSetBase,
+):
+    lookup_field = "name"
+    lookup_url_kwarg = "name"
+    lookup_value_regex = r"[^/]+"
 
 
 #

nautobot/extras/tests/test_api.py
@@ -1118,6 +1118,20 @@ class JobTest(
             {"name": "var4", "type": "ObjectVar", "required": True, "model": "extras.role"},
         )
 
+    def test_get_job_variables_by_name(self):
+        """Test the job/<name>/variables API endpoint."""
+        self.add_permissions("extras.view_job")
+        route = get_route_for_model(self.model, "variables", api=True)
+        response = self.client.get(reverse(route, kwargs={"name": self.job_model.name}), **self.header)
+        self.assertEqual(4, len(response.data))  # 4 variables, in order
+        self.assertEqual(response.data[0], {"name": "var1", "type": "StringVar", "required": True})
+        self.assertEqual(response.data[1], {"name": "var2", "type": "IntegerVar", "required": True})
+        self.assertEqual(response.data[2], {"name": "var3", "type": "BooleanVar", "required": False})
+        self.assertEqual(
+            response.data[3],
+            {"name": "var4", "type": "ObjectVar", "required": True, "model": "extras.role"},
+        )
+
     @override_settings(EXEMPT_VIEW_PERMISSIONS=["*"])
     def test_update_job_with_sensitive_variables_set_approval_required_to_true(self):
         job_model = Job.objects.get_for_class_path("api_test_job.APITestJob")

nautobot/core/settings.py
@@ -240,6 +240,10 @@ SPECTACULAR_SETTINGS = {
     # use sidecar - locally packaged UI files, not CDN
     "SWAGGER_UI_DIST": "SIDECAR",
     "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "displayOperationId": True,
+    },
     "REDOC_DIST": "SIDECAR",
     # Do not list all possible enum values in the description of filter fields and the like
     # In addition to being highly verbose, it's inaccurate for filter fields like *__ic and *__re

nautobot/extras/api/urls.py
@@ -42,6 +42,7 @@ router.register("image-attachments", views.ImageAttachmentViewSet)
 
 # Jobs
 router.register("jobs", views.JobViewSet)
+router.register("jobs", views.JobByNameViewSet)
 
 # Job Buttons
 router.register("job-buttons", views.JobButtonViewSet)

commit 02b28364ac403e0bb1fc45f18e1a51b4579dc95c

Fix regression introduced in #4517 and add test coverage (#4524)
* Fix regression introduced in #4476 and add test coverage

* Update change fragment

* Update changes/4524.fixed

Co-authored-by: Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>

---------

Co-authored-by: Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>

nautobot/extras/api/views.py
@@ -80,18 +80,37 @@ class ExtrasRootView(APIRootView):
 
 
 class NotesViewSetMixin:
+    def restrict_queryset(self, request, *args, **kwargs):
+        """
+        Apply "view" permissions on the POST /notes/ endpoint, otherwise as ModelViewSetMixin.
+        """
+        if request.user.is_authenticated and self.action == "notes":
+            self.queryset = self.queryset.restrict(request.user, "view")
+        else:
+            super().restrict_queryset(request, *args, **kwargs)
+
+    class CreateNotePermissions(TokenPermissions):
+        """As nautobot.core.api.authentication.TokenPermissions, but enforcing add_note permission."""
+
+        perms_map = {
+            "GET": ["%(app_label)s.view_%(model_name)s", "extras.view_note"],
+            "POST": ["%(app_label)s.view_%(model_name)s", "extras.add_note"],
+        }
+
     @extend_schema(methods=["get"], filters=False, responses={200: serializers.NoteSerializer(many=True)})
     @extend_schema(
         methods=["post"],
         request=serializers.NoteInputSerializer,
         responses={201: serializers.NoteSerializer(many=False)},
     )
-    @action(detail=True, url_path="notes", methods=["get", "post"])
+    @action(detail=True, url_path="notes", methods=["get", "post"], permission_classes=[CreateNotePermissions])
     def notes(self, request, *args, **kwargs):
         """
         API methods for returning or creating notes on an object.
         """
-        obj = get_object_or_404(self.queryset, **{self.lookup_field: self.kwargs[self.lookup_url_kwarg]})
+        obj = get_object_or_404(
+            self.queryset, **{self.lookup_field: self.kwargs[self.lookup_url_kwarg or self.lookup_field]}
+        )
         if request.method == "POST":
             content_type = ContentType.objects.get_for_model(obj)
             data = request.data
@@ -116,7 +135,7 @@ class NotesViewSetMixin:
 #
 
 
-class ComputedFieldViewSet(ModelViewSet, NotesViewSetMixin):
+class ComputedFieldViewSet(NotesViewSetMixin, ModelViewSet):
     """
     Manage Computed Fields through DELETE, GET, POST, PUT, and PATCH requests.
     """
@@ -153,7 +172,7 @@ class ConfigContextQuerySetMixin:
         return queryset
 
 
-class ConfigContextViewSet(ModelViewSet, NotesViewSetMixin):
+class ConfigContextViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = ConfigContext.objects.prefetch_related(
         "locations",
         "roles",
@@ -171,7 +190,7 @@ class ConfigContextViewSet(ModelViewSet, NotesViewSetMixin):
 #
 
 
-class ConfigContextSchemaViewSet(ModelViewSet, NotesViewSetMixin):
+class ConfigContextSchemaViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = ConfigContextSchema.objects.all()
     serializer_class = serializers.ConfigContextSchemaSerializer
     filterset_class = filters.ConfigContextSchemaFilterSet
@@ -198,7 +217,7 @@ class ContentTypeViewSet(viewsets.ReadOnlyModelViewSet):
 #
 
 
-class CustomFieldViewSet(ModelViewSet, NotesViewSetMixin):
+class CustomFieldViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = CustomField.objects.all()
     serializer_class = serializers.CustomFieldSerializer
     filterset_class = filters.CustomFieldFilterSet
@@ -229,7 +248,7 @@ class CustomFieldModelViewSet(ModelViewSet):
         return context
 
 
-class NautobotModelViewSet(CustomFieldModelViewSet, NotesViewSetMixin):
+class NautobotModelViewSet(NotesViewSetMixin, CustomFieldModelViewSet):
     """Base class to use for API ViewSets based on OrganizationalModel or PrimaryModel.
 
     Can also be used for models derived from BaseModel, so long as they support Notes.
@@ -241,7 +260,7 @@ class NautobotModelViewSet(CustomFieldModelViewSet, NotesViewSetMixin):
 #
 
 
-class CustomLinkViewSet(ModelViewSet, NotesViewSetMixin):
+class CustomLinkViewSet(NotesViewSetMixin, ModelViewSet):
     """
     Manage Custom Links through DELETE, GET, POST, PUT, and PATCH requests.
     """
@@ -256,7 +275,7 @@ class CustomLinkViewSet(ModelViewSet, NotesViewSetMixin):
 #
 
 
-class DynamicGroupViewSet(ModelViewSet, NotesViewSetMixin):
+class DynamicGroupViewSet(NotesViewSetMixin, ModelViewSet):
     """
     Manage Dynamic Groups through DELETE, GET, POST, PUT, and PATCH requests.
     """
@@ -296,7 +315,7 @@ class DynamicGroupMembershipViewSet(ModelViewSet):
 #
 
 
-class ExportTemplateViewSet(ModelViewSet, NotesViewSetMixin):
+class ExportTemplateViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = ExportTemplate.objects.all()
     serializer_class = serializers.ExportTemplateSerializer
     filterset_class = filters.ExportTemplateFilterSet
@@ -338,7 +357,7 @@ class GitRepositoryViewSet(NautobotModelViewSet):
 #
 
 
-class GraphQLQueryViewSet(ModelViewSet, NotesViewSetMixin):
+class GraphQLQueryViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = GraphQLQuery.objects.all()
     serializer_class = serializers.GraphQLQuerySerializer
     filterset_class = filters.GraphQLQueryFilterSet
@@ -436,9 +455,9 @@ class JobViewSetBase(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
+    NotesViewSetMixin,
     ModelViewSetMixin,
     viewsets.GenericViewSet,
-    NotesViewSetMixin,
 ):
     queryset = Job.objects.all()
     serializer_class = serializers.JobSerializer
@@ -752,7 +771,7 @@ class JobResultViewSet(
 #
 
 
-class JobButtonViewSet(ModelViewSet, NotesViewSetMixin):
+class JobButtonViewSet(NotesViewSetMixin, ModelViewSet):
     """
     Manage Job Buttons through DELETE, GET, POST, PUT, and PATCH requests.
     """
@@ -937,7 +956,7 @@ class ObjectChangeViewSet(ReadOnlyModelViewSet):
 #
 
 
-class RelationshipViewSet(ModelViewSet, NotesViewSetMixin):
+class RelationshipViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = Relationship.objects.all()
     serializer_class = serializers.RelationshipSerializer
     filterset_class = filters.RelationshipFilterSet
@@ -1051,7 +1070,7 @@ class TagViewSet(NautobotModelViewSet):
 #
 
 
-class WebhooksViewSet(ModelViewSet, NotesViewSetMixin):
+class WebhooksViewSet(NotesViewSetMixin, ModelViewSet):
     """
     Manage Webhooks through DELETE, GET, POST, PUT, and PATCH requests.
     """

nautobot/extras/models/mixins.py
@@ -124,9 +124,9 @@ class NotesMixin:
 
         return self._notes_queryset
 
-    def get_notes_url(self):
+    def get_notes_url(self, api=False):
         """Return the notes URL for a given instance."""
-        route = get_route_for_model(self, "notes")
+        route = get_route_for_model(self, "notes", api=api)
 
         # Iterate the pk-like fields and try to get a URL, or return None.
         fields = ["pk", "slug"]

nautobot/virtualization/api/views.py
@@ -77,7 +77,7 @@ class VirtualMachineViewSet(ConfigContextQuerySetMixin, NautobotModelViewSet):
     filterset_class = filters.VirtualMachineFilterSet
 
 
-class VMInterfaceViewSet(ModelViewSet, NotesViewSetMixin):
+class VMInterfaceViewSet(NotesViewSetMixin, ModelViewSet):
     queryset = VMInterface.objects.select_related(
         "virtual_machine",
         "parent_interface",

nautobot/core/testing/api.py
@@ -5,7 +5,7 @@ from typing import Optional, Sequence, Union
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import ForeignKey, ManyToManyField
+from django.db.models import ForeignKey, ManyToManyField, QuerySet
 from django.test import override_settings, tag
 from django.urls import reverse
 from django.utils.text import slugify
@@ -1032,23 +1032,62 @@ class APIViewTestCases:
 
         @override_settings(EXEMPT_VIEW_PERMISSIONS=[])
         def test_notes_url_on_object(self):
-            notes = getattr(self.model, "notes", None)
-            if notes and isinstance(notes, ForeignKey):
-                instance1 = self._get_queryset().first()
-                # Add object-level permission
-                obj_perm = users_models.ObjectPermission(
-                    name="Test permission",
-                    constraints={"pk": instance1.pk},
-                    actions=["view"],
-                )
-                obj_perm.save()
-                obj_perm.users.add(self.user)
-                obj_perm.object_types.add(ContentType.objects.get_for_model(self.model))
-                url = self._get_detail_url(instance1)
-                response = self.client.get(url, **self.header)
-                self.assertHttpStatus(response, status.HTTP_200_OK)
-                self.assertIn("notes_url", response.data)
-                self.assertIn(f"{url}notes/", str(response.data["notes_url"]))
+            if not hasattr(self.model, "notes"):
+                self.skipTest("Model doesn't appear to support Notes")
+            instance = self._get_queryset().first()
+            if not isinstance(instance.notes, QuerySet):
+                self.skipTest("Model has a notes field but it doesn't appear to be Notes")
+
+            # Add object-level permission
+            obj_perm = users_models.ObjectPermission(
+                name="Test permission",
+                constraints={"pk": instance.pk},
+                actions=["view"],
+            )
+            obj_perm.save()
+            obj_perm.users.add(self.user)
+            obj_perm.object_types.add(ContentType.objects.get_for_model(self.model))
+            url = self._get_detail_url(instance)
+            response = self.client.get(url, **self.header)
+            self.assertHttpStatus(response, status.HTTP_200_OK)
+            self.assertIn("notes_url", response.data)
+            self.assertIn(f"{url}notes/", str(response.data["notes_url"]))
+            self.assertIn(instance.get_notes_url(api=True), str(response.data["notes_url"]))
+
+        @override_settings(EXEMPT_VIEW_PERMISSIONS=[])
+        def test_notes_url_functionality(self):
+            if not hasattr(self.model, "notes"):
+                self.skipTest("Model doesn't appear to support Notes")
+            instance = self._get_queryset().first()
+            if not isinstance(instance.notes, QuerySet):
+                self.skipTest("Model has a notes field but it doesn't appear to be Notes")
+
+            self.add_permissions(f"{self.model._meta.app_label}.view_{self.model._meta.model_name}")
+            self.add_permissions("extras.add_note")
+
+            # Add note via REST API
+            notes_url = instance.get_notes_url(api=True)
+            response = self.client.post(
+                notes_url,
+                {"note": f"This is a note for {instance}"},
+                format="json",
+                **self.header,
+            )
+            self.assertHttpStatus(response, status.HTTP_201_CREATED)
+            self.assertIsInstance(response.data, dict)
+            self.assertEqual(f"This is a note for {instance}", response.data["note"])
+            self.assertEqual(str(self.user.pk), str(response.data["user"]["id"]))
+            self.assertEqual(str(instance.pk), str(response.data["assigned_object_id"]))
+            self.assertEqual(str(instance.pk), str(response.data["assigned_object"]["id"]))
+
+            # Get note via REST API
+            response = self.client.get(notes_url, format="json", **self.header)
+            self.assertHttpStatus(response, status.HTTP_403_FORBIDDEN)
+
+            self.add_permissions("extras.view_note")
+            response = self.client.get(notes_url, format="json", **self.header)
+            self.assertHttpStatus(response, status.HTTP_200_OK)
+            self.assertEqual(f"This is a note for {instance}", response.data["results"][0]["note"])
 
     class TreeModelAPIViewTestCaseMixin:
         """Test `?depth=2` query parameter for TreeModel"""

nautobot/extras/tests/test_api.py
@@ -1052,6 +1052,7 @@ class JobTest(
     APIViewTestCases.ListObjectsViewTestCase,
     APIViewTestCases.UpdateObjectViewTestCase,
     APIViewTestCases.DeleteObjectViewTestCase,
+    APIViewTestCases.NotesURLViewTestCase,
 ):
     """Test cases for the Jobs REST API."""
 